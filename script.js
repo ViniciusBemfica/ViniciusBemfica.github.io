0af4b5ea26675968ac5105cb96273b8f0ccd89a1
ViniciusBemfica <viniciusbemfica@hotmail.com>
2024-04-04 18:01:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ function AtualizaInfosConringas(){
     document.getElementById("InforNomeCoringa1").textContent = arrayOfCoringa[pIdCoringa1].Nome;
     document.getElementById("InforNomeCoringa2").textContent = arrayOfCoringa[pIdCoringa2].Nome;
     document.getElementById("InforNomeCoringa3").textContent = arrayOfCoringa[pIdCoringa3].Nome;
-    document.getElementById("InforCoringa1").textContent = arrayOfCoringa[pIdCoringa1].Id + ' - ' + arrayOfCoringa[pIdCoringa1].Descricao;
+    document.getElementById("InforCoringa1").innerHTML = arrayOfCoringa[pIdCoringa1].Id + ' - ' + arrayOfCoringa[pIdCoringa1].Descricao;
     document.getElementById("InforCoringa2").textContent = arrayOfCoringa[pIdCoringa2].Id + ' - ' + arrayOfCoringa[pIdCoringa2].Descricao;  
     document.getElementById("InforCoringa3").textContent = arrayOfCoringa[pIdCoringa3].Id + ' - ' + arrayOfCoringa[pIdCoringa3].Descricao; 
 }
